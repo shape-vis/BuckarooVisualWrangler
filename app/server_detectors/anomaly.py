c83a858456dc519e531f93f88b09aaf84e539424
reporterfredbriggs <nickbaret5@gmail.com>
2025-06-11 09:19:30 -0600

    added sample size request to the getsample fetch from db

--- app/server_detectors/anomaly.py
+++
@@ -1,9 +0,0 @@
-"""
-Detector which populates a map of cells which have values that have a z-score
-greater than 2
-"""
-import pandas as pd
-
-
-def detect_anomaly(dataset):
-    dataframe = pd.DataFrame(dataset)
