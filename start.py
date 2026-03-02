d77cdca2f5dd6a134c5239158cef827f15eea5c7
reporterfredbriggs <nickbaret5@gmail.com>
2026-03-01 22:25:56 -0700

    full-stack debugging set-up

--- start.py
+++ start.py
@@ -1,8 +1,8 @@
 #Buckaroo Project - June 1, 2025
 #This file starts the app
 
-
 from app import app
 
-if __name__ == 'main':
-    app.run( )
+if __name__ == "__main__":
+    print("INSIDE GUARD")
+    app.run(host="127.0.0.1", port=5001, debug=True, use_reloader=False)
