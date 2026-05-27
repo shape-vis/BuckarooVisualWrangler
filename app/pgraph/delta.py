7d58b86e597a468cb5b487d85a0f7664f512e4da
arnavg24-arch <arnavg24@vt.edu>
2026-05-26 23:01:11 -0700

    feat: add delta class structure for tracking state changes

--- app/pgraph/delta.py
+++ app/pgraph/delta.py
@@ -1,15 +1,23 @@
 from typing import Dict, Any, List
 
+"""
+Defines a delta object that represents a single wrangling operation (a delta).
+"""
+
 class Delta:
-    """
-    Represents a single wrangling operation (a delta).
-    """
+
     def __init__(self, operation: str, parameters: Dict[str, Any], pandas_code: str = ""):
+        """
+        This is the definition method for the Delta class. It is used to create a new delta object.
+        """
         self.operation = operation
         self.parameters = parameters
         self.pandas_code = pandas_code
 
     def __json__(self):
+        """
+        This is the json method for the Delta class. It is used to convert a delta object to a json object.
+        """
         return {
             "operation": self.operation,
             "parameters": self.parameters,
@@ -18,6 +26,9 @@ class Delta:
 
     @staticmethod
     def from_dict(data: Dict[str, Any]):
+        """
+        This is the from_dict method for the Delta class. It is used to create a delta object from a dictionary.
+        """
         return Delta(
             operation=data.get("operation", "unknown"),
             parameters=data.get("parameters", {}),
