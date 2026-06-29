69d360187ce0a9b0a38c913b15f9570443275707
marikmartinez <mk727@hawaii.edu>
2026-06-29 13:22:28 -0600

    Created DataProfile class (still a WIP)

--- app/ai_utils/data_attributes.py
+++ app/ai_utils/data_attributes.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import json
 
-from app.server_utils.service_helpers import is_categorical, get_error_dist
+from pandas.core.arrays import categorical
+
 from app.db_utils.execute_sql import fetch_sql
 
 
@@ -19,23 +20,41 @@ def load_table_to_df(table_name, engine):
 
     return df
 
+def to_scalar(val):
+    if val is None:
+        return None
+    if isinstance(val, pd.DataFrame):
+        if val.empty:
+            return None
+        return to_scalar(val.iloc[0, 0])  # first row, first column
+    if isinstance(val, pd.Series):
+        return None if val.empty else to_scalar(val.iloc[0])
+    if isinstance(val, (list, tuple)) and len(val) > 0:
+        return to_scalar(val[0])
+    if hasattr(val, 'item'):
+        return val.item()
+    return val
 
 class DataProfile:
     # TODO: add a way to figure out where the changes were made (more efficient updating of data attributes
     def __init__(self, table_name, engine=None, main_df=None, error_df=None):
         self.table_name = table_name
+        self.data_profile_table_name = "data_profile_" + table_name
         self.engine = engine
         self.main_df = main_df
         self.error_df = error_df
+        self.default_attributes = ['mean', 'median', 'min', 'max', 'n_categories', 'mode']
         self.name_to_func = {
             'mean': self.calculate_mean,
             'median': self.calculate_median,
             'min': self.calculate_min,
-            'max': self.calculate_max #TODO: add more
+            'max': self.calculate_max, #TODO: add more,
+            'n_categories': self.calculate_num_categories,
+            'mode': self.calculate_mode
         }
         self.attribute_type_assignment = { # TODO: fix this name it sucks lol
-            'numeric': ['mean', 'median', 'min', 'max'],
-            'categorical': ['n_categories'
+            'numeric': ['mean', 'median', 'min', 'max', 'mode'],
+            'categorical': ['n_categories',
                             'mode']
         }
 
@@ -48,27 +67,56 @@ class DataProfile:
             assert engine is not None, f"engine cannot be None if error_df is None"
             self.error_df = load_table_to_df(f"errors_{table_name}", engine)
 
+
+    def get_col_data(self, column_name, attribute_name):
+        assert (attribute_name in self.attribute_type_assignment['categorical'] or attribute_name in self.attribute_type_assignment['numeric']), f"Invalid attribute name {attribute_name}"
+
+        if attribute_name in self.attribute_type_assignment['categorical']:
+            col_data = self.main_df[column_name].fillna('N/A')
+        if attribute_name in self.attribute_type_assignment['numeric']:
+            col_data = pd.to_numeric(self.main_df[column_name], errors='coerce').dropna()
+
+        return col_data
+
+
+
     def look_up_stat_from_profile(self, attribute_name, column_name):
+        data_profile_table_name = "dp_" + self.table_name
+        try:
+            query = f'SELECT {attribute_name} FROM "{data_profile_table_name}" WHERE column_name = "{column_name}"'
+            stat = fetch_sql(query, False, self.engine)
+
+            # TODO: make sure that when nothing matches the query, this still will work
+            if stat is None:
+                assert False, "AHHHHHHHHHHHHHHHHHHHHHHHHHHHH THIS SHOULDNT BE HAPPENING!!!!!!!!!"
+            elif stat.empty: # TODO: idk what I'm doing here
+                return None
+            return stat
+        except Exception as e:
+            print(f"Attribute {attribute_name} not found from data profile.")
+
 
 
     def query_summary_stat_from_main_df(self, stat_query, column_name):
         # TODO: fix this probably
-        query = f'SELECT {stat_query}({column_name}) FROM "{self.table_name}"'
-        stat = fetch_sql(query, False, self.engine)
+        query = f'SELECT {stat_query}("{column_name}") FROM "{self.table_name}"'
+        stat = fetch_sql(query, True, self.engine)
         return stat
 
 
-    def calculate_column_attribute(self, attribute_name, column_name):
-        look_up_value = self.look_up_stat(self, attribute_name, column_name)
-        # TODO: add asserts to make sure that attribute is compatible with the type of the column
-        if look_up_value is not None:
-            return look_up_value
+    def calculate_column_attribute(self, attribute_name, column_name, look_up_stat=True):
+        if look_up_stat:
+            look_up_value = self.look_up_stat_from_profile(attribute_name, column_name)
+
+            if look_up_value is not None:
+                return to_scalar(look_up_value)
 
         calculate_attribute_func = self.name_to_func[attribute_name]
-        return calculate_attribute_func(column_name)
+        return to_scalar(calculate_attribute_func(column_name))
 
     # TODO: save stat off to table if newly calculated
     def calculate_mean(self, column_name):
+        col_data = self.get_col_data(column_name, 'mean')
         try:
             avg = self.query_summary_stat_from_main_df('AVG', column_name)
         except Exception as e:
@@ -76,13 +124,15 @@ class DataProfile:
 
             print("Calculating mean manually using data...")
 
-            avg = self.main_df[column_name].mean()
+            avg = col_data.mean()
             print(f"Updating mean value for column {column_name} in data profile")
 
         return avg
 
 
     def calculate_median(self, column_name):
+        col_data = self.get_col_data(column_name, 'median')
+
         try:
             median = self.query_summary_stat_from_main_df('MEDIAN', column_name)
         except Exception as e:
@@ -90,7 +140,7 @@ class DataProfile:
 
             print("Calculating median manually using data...")
 
-            median = self.main_df[column_name].median()
+            median = col_data.median()
 
             print(f"Updating median value for column {column_name} in data profile")
 
@@ -98,6 +148,7 @@ class DataProfile:
 
 
     def calculate_max(self, column_name):
+        col_data = self.get_col_data(column_name, 'max')
         try:
             maximum = self.query_summary_stat_from_main_df('MAX', column_name)
         except Exception as e:
@@ -105,12 +156,15 @@ class DataProfile:
 
             print("Calculating maximum manually using data...")
 
-            maximum = self.main_df[column_name].max()
+            maximum = col_data.max()
 
             print(f"Updating maximum value for column {column_name} in data profile")
+
         return maximum
 
     def calculate_min(self, column_name):
+
+        col_data = self.get_col_data(column_name, 'min')
         try:
             minimum = self.query_summary_stat_from_main_df('MIN', column_name)
         except Exception as e:
@@ -118,22 +172,23 @@ class DataProfile:
 
             print("Calculating minimum manually using data...")
 
-            minimum = self.main_df[column_name].min()
+            minimum = col_data.min()
 
             print(f"Updating minimum value for column {column_name} in data profile")
 
         return minimum
 
     def calculate_num_categories(self, column_name):
+        col_data = self.get_col_data(column_name, 'n_categories')
         try:
             query = f'SELECT COUNT(DISTINCT "{column_name}") FROM "{self.table_name}"'
-            n_categories = fetch_sql(query, False, self.engine)
+            n_categories = fetch_sql(query, True, self.engine)
         except Exception as e:
             print(f"Error fetching the n_categories for table {self.table_name} at column {column_name}: {e}")
 
             print("Calculating n_categories manually using data...")
 
-            n_categories = self.main_df[column_name].nunique()
+            n_categories = col_data.nunique()
 
             print(f"Updating n_categories value for column {column_name} in data profile")
 
@@ -142,11 +197,12 @@ class DataProfile:
 
     def calculate_mode(self, column_name):
         print("Calculating mode manually using data...")
-        mode = self.main_df[column_name].mode()
+        col_data = self.get_col_data(column_name, 'mode')
+        mode = col_data.mode()
 
         return mode
 
-
+    # TODO: calculate some stats relating to the error df
 
 
 
